043f214bd1e73c49ded5aaf8f2175a07cfda8a3c
Pooja Karkhile <86223323+pooja1923@users.noreply.github.com>
2025-01-02 22:55:13 +0530

    Create GlobalExceptionHandler.java

---
+++ src/main/java/com/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<String> handleCustomException(CustomException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
