3a682fa4dbb25c19457ba30c27404dd31ab43f0f
Pooja Karkhile <86223323+pooja1923@users.noreply.github.com>
2025-01-02 22:54:46 +0530

    Create CustomException.java

---
+++ src/main/java/com/CustomException.java
@@ -0,0 +1,16 @@
+package com;
+
+public class CustomException extends RuntimeException {
+    
+    private String message;
+    
+    public CustomException(String message) {
+        super(message);
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
