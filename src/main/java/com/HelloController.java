44b33b738edb501e0285a7802da102b0c52972e5
Pooja Karkhile <86223323+pooja1923@users.noreply.github.com>
2025-01-02 22:53:42 +0530

    Update HelloController.java

--- src/main/java/com/HelloController.java
+++ src/main/java/com/HelloController.java
@@ -1,5 +1,6 @@
 package com;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +8,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class HelloController {
-	
-	@GetMapping("/welcome")
-	public String welcome()
-	{
-		return "Code is deployed in Code Deploy Again";
-	}
 
+    private final HelloService helloService;
+
+    @Autowired
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return helloService.getWelcomeMessage();
+    }
+
+    @GetMapping("/goodbye")
+    public String goodbye() {
+        return helloService.getGoodbyeMessage();
+    }
+
+    @GetMapping("/error")
+    public String error() {
+        throw new CustomException("Custom error occurred!");
+    }
 }
