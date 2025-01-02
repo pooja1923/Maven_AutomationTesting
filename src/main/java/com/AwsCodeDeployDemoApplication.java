ccecacac83c4ec2bf20c31ea63361eac25ed3186
Pooja Karkhile <86223323+pooja1923@users.noreply.github.com>
2025-01-02 22:53:12 +0530

    Update AwsCodeDeployDemoApplication.java

--- src/main/java/com/AwsCodeDeployDemoApplication.java
+++ src/main/java/com/AwsCodeDeployDemoApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AwsCodeDeployDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AwsCodeDeployDemoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(AwsCodeDeployDemoApplication.class, args);
+    }
 }
