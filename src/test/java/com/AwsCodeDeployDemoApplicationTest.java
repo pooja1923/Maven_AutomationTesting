7ea9219ee9ed001737d9db2481e3fe74cce5a009
Pooja Karkhile <86223323+pooja1923@users.noreply.github.com>
2025-01-03 13:57:19 +0530

    Create AwsCodeDeployDemoApplicationTest.java

---
+++ src/test/java/com/AwsCodeDeployDemoApplicationTest.java
@@ -0,0 +1,14 @@
+package com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsCodeDeployDemoApplicationTest {
+
+    @Test
+    void contextLoads() {
+        // This test will attempt to load the Spring application context
+        // If there are any issues in starting the app, this test will fail
+    }
+}
