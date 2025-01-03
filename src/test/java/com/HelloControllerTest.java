d55ab4c5edf6fa6bf6a3f4d9777dcb3509f04cfd
Pooja Karkhile <86223323+pooja1923@users.noreply.github.com>
2025-01-03 12:04:32 +0530

    Update HelloControllerTest.java

--- src/test/java/com/HelloControllerTest.java
+++ src/test/java/com/HelloControllerTest.java
@@ -2,7 +2,7 @@ package com;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -13,7 +13,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@WebMvcTest(HelloController.class)
 class HelloControllerTest {
 
     @Autowired
