820acb363f46623614a14c227ec99ae81ec767df
Pooja Karkhile <86223323+pooja1923@users.noreply.github.com>
2025-01-03 12:31:59 +0530

    Update HelloControllerTest.java

--- src/test/java/com/HelloControllerTest.java
+++ src/test/java/com/HelloControllerTest.java
@@ -1,39 +1,42 @@
 package com;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@SpringBootTest
-public class HelloControllerTest {
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HelloController.class)
+class HelloControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @MockBean
-    private HelloService helloService;  // Mock the service that is injected into the controller
+    private HelloService helloService;
 
     @Test
-    public void testWelcomeEndpoint() throws Exception {
-        // Mock the service method
+    void testWelcome() throws Exception {
         when(helloService.getWelcomeMessage()).thenReturn("Code is deployed in Code Deploy Again");
 
-        String response = mockMvc.perform(MockMvcRequestBuilders.get("/api/welcome"))
-                .andReturn().getResponse().getContentAsString();
-        assertEquals("Code is deployed in Code Deploy Again", response);
+        mockMvc.perform(get("/api/welcome").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Code is deployed in Code Deploy Again")));
     }
 
     @Test
-    public void testGoodbyeEndpoint() throws Exception {
-        // Mock the service method
-        when(helloService.getGoodbyeMessage()).thenReturn("Goodbye, see you again!");
+    void testGoodbye() throws Exception {
+        when(helloService.getGoodbyeMessage()).thenReturn("Goodbye! See you next time!");
 
-        String response = mockMvc.perform(MockMvcRequestBuilders.get("/api/goodbye"))
-                .andReturn().getResponse().getContentAsString();
-        assertEquals("Goodbye, see you again!", response);
+        mockMvc.perform(get("/api/goodbye").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Goodbye! See you next time!")));
     }
 }
