b9fb2a1a1f842c105bfb893e0041b9403b6eb7e5
Pooja Karkhile <86223323+pooja1923@users.noreply.github.com>
2025-01-03 13:31:04 +0530

    Create TestException.java

---
+++ src/main/java/com/TestException.java
@@ -0,0 +1,12 @@
+package com;
+
+public class TestException extends RuntimeException {
+
+    public TestException(String message) {
+        super(message);
+    }
+
+    public TestException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
