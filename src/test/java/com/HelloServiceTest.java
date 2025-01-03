9ca4c912f97637e7ad0ea26140e3ac26b35664dc
Pooja Karkhile <86223323+pooja1923@users.noreply.github.com>
2025-01-03 13:20:34 +0530

    Create HelloServiceTest.java

---
+++ src/test/java/com/HelloServiceTest.java
@@ -0,0 +1,34 @@
+package com;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class HelloServiceTest {
+
+    private final HelloService helloService = new HelloService();
+
+    @Test
+    void testGetWelcomeMessage() {
+        assertEquals("Code is deployed in Code Deploy Again", helloService.getWelcomeMessage());
+    }
+
+    @Test
+    void testGetGoodbyeMessage() {
+        assertEquals("Goodbye, see you again!", helloService.getGoodbyeMessage());
+    }
+
+    @Test
+    void testGetHelloMessage() {
+        assertEquals("Hello from AWS CodeDeploy Demo!", helloService.getHelloMessage());
+    }
+
+    @Test
+    void testSimulateError() {
+        CustomException thrown = assertThrows(CustomException.class, () -> {
+            helloService.simulateError();
+        });
+        assertEquals("Simulated service error!", thrown.getMessage());
+    }
+}
