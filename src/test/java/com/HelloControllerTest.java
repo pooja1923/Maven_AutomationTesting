876eea5f00a82ec6df497cc24d6db1725f0e2315
Pooja Karkhile <86223323+pooja1923@users.noreply.github.com>
2025-01-03 12:16:52 +0530

    Update HelloControllerTest.java

--- src/test/java/com/HelloControllerTest.java
+++ src/test/java/com/HelloControllerTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -13,8 +14,14 @@ public class HelloControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private HelloService helloService;  // Mock the service that is injected into the controller
+
     @Test
     public void testWelcomeEndpoint() throws Exception {
+        // Mock the service method
+        when(helloService.getWelcomeMessage()).thenReturn("Code is deployed in Code Deploy Again");
+
         String response = mockMvc.perform(MockMvcRequestBuilders.get("/api/welcome"))
                 .andReturn().getResponse().getContentAsString();
         assertEquals("Code is deployed in Code Deploy Again", response);
@@ -22,6 +29,9 @@ public class HelloControllerTest {
 
     @Test
     public void testGoodbyeEndpoint() throws Exception {
+        // Mock the service method
+        when(helloService.getGoodbyeMessage()).thenReturn("Goodbye, see you again!");
+
         String response = mockMvc.perform(MockMvcRequestBuilders.get("/api/goodbye"))
                 .andReturn().getResponse().getContentAsString();
         assertEquals("Goodbye, see you again!", response);
